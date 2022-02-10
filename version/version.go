46fbd5d00b6cc09c24d81d89ee72bb0c27df58bf
hunjixin <1084400399@qq.com>
2022-02-10 16:50:15 +0800

    bump version to v1.2.0-rc2

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ import "fmt"
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "v1.2.0-rc1"
+const BuildVersion = "v1.2.0-rc2"
 
 var UserVersion = BuildVersion + CurrentCommit
 
