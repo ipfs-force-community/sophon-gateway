a9c03a1a8bbb3a3421b3d646572415024815fe43
hunjixin <1084400399@qq.com>
2022-02-18 16:46:40 +0800

    upgdate version

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ import "fmt"
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "v1.2.0-rc2"
+const BuildVersion = "v1.2.0-rc3"
 
 var UserVersion = BuildVersion + CurrentCommit
 
