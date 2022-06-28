727a393fd7b198e7ae4470c5649cc4d1c3b44344
tom <69969590+simlecode@users.noreply.github.com>
2022-06-28 14:35:12 +0800

    chore: bump version to v1.6.0 (#61)
    * chore: upgrade venus & venus-auth
    
    * chore: bump version to v1.6.0

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ import "fmt"
 var GitCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "v1.6.0-rc2"
+const BuildVersion = "v1.6.0"
 
 var UserVersion = BuildVersion + GitCommit
 
