cc5122b409854bdb011481dba5fe2099d0f88604
hunjixin <1084400399@qq.com>
2022-10-17 14:40:09 +0800

    feat: release version 1.8.0 rc1

--- version/version.go
+++ version/version.go
@@ -3,6 +3,6 @@ package version
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "v1.7.0"
+const BuildVersion = "v1.8.0-rc1"
 
 var UserVersion = BuildVersion + CurrentCommit
