2ca40c767f2a559d0d0e699ddd7a3ed659373397
tom <69969590+simlecode@users.noreply.github.com>
2022-11-03 11:42:18 +0800

    chore: bump version to v1.8.0-rc2 (#96)
    * chore: update venus & venus-auth
    
    * chore: bump version to v1.8.0-rc2

--- version/version.go
+++ version/version.go
@@ -3,6 +3,6 @@ package version
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "v1.8.0-rc1"
+const BuildVersion = "v1.8.0-rc2"
 
 var UserVersion = BuildVersion + CurrentCommit
