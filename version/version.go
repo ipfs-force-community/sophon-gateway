c04167afb002fd4767d40166d78284ac2f3ce0c4
Tiance <1033935631@qq.com>
2022-12-30 11:28:11 +0800

    chore: bump version to 1.9.0

--- version/version.go
+++ version/version.go
@@ -3,6 +3,6 @@ package version
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.9.0-rc1"
+const BuildVersion = "1.9.0"
 
 var UserVersion = BuildVersion + CurrentCommit
