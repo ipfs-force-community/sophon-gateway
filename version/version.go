237e51c35be3e5b291255769ee6a0f61f11ad614
tanlang <lin.zexiao@qq.com>
2023-04-24 14:12:04 +0800

    chore: bump up version to v1.11.1

--- version/version.go
+++ version/version.go
@@ -3,6 +3,6 @@ package version
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.11.0"
+const BuildVersion = "1.11.1"
 
 var UserVersion = BuildVersion + CurrentCommit
