fef7f4468cb9bd384a4cea3a2ecacf7e1747661d
simlecode <69969590+simlecode@users.noreply.github.com>
2023-06-13 16:26:04 +0800

    chore: bump version to v1.12.0-rc1

--- version/version.go
+++ version/version.go
@@ -3,6 +3,6 @@ package version
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.11.1"
+const BuildVersion = "1.12.0-rc1"
 
 var UserVersion = BuildVersion + CurrentCommit
